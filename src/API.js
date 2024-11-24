2205cc84f8d5f44398c3c7bb95368a81f13477bf
Ahtisham Ul Haq <77314350+ahtisham774@users.noreply.github.com>
2024-11-24 17:40:04 +0500

    update API

--- src/API.js
+++ src/API.js
@@ -1,3 +1,4 @@
-const URI = 'http://localhost:8080';
+// const URI = 'http://localhost:8080';
+const URI = 'https://rogue-mewing-krill.glitch.me';
 export const BASE_URL = URI ;
 export const AUTH = `${BASE_URL}/auth`;
